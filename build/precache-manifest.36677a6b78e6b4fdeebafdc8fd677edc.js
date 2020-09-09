21f865c0d8d478b3d313a4ffd0b0b873ecf58249
DaniilGorokhov <kripa0cool@mail.ru>
2020-09-10 01:04:01 +0300

    fixes github pages

---
+++ build/precache-manifest.36677a6b78e6b4fdeebafdc8fd677edc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6398d357ce35a3cf8b2ea9cc5c2edf6c",
+    "url": "/DaniilGorokhov.github.io/index.html"
+  },
+  {
+    "revision": "af6d58fc66d602036916",
+    "url": "/DaniilGorokhov.github.io/static/css/main.a1107254.chunk.css"
+  },
+  {
+    "revision": "2acb1253f70f1995a8b8",
+    "url": "/DaniilGorokhov.github.io/static/js/2.95af18a6.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/DaniilGorokhov.github.io/static/js/2.95af18a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6d58fc66d602036916",
+    "url": "/DaniilGorokhov.github.io/static/js/main.77abd3cb.chunk.js"
+  },
+  {
+    "revision": "5839fa1b49b1d26da03e",
+    "url": "/DaniilGorokhov.github.io/static/js/runtime-main.00e04010.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/DaniilGorokhov.github.io/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/DaniilGorokhov.github.io/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/DaniilGorokhov.github.io/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/DaniilGorokhov.github.io/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/DaniilGorokhov.github.io/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/DaniilGorokhov.github.io/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/DaniilGorokhov.github.io/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/DaniilGorokhov.github.io/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/DaniilGorokhov.github.io/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/DaniilGorokhov.github.io/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/DaniilGorokhov.github.io/static/media/sub.4eb05fdd.png"
+  }
+]);
