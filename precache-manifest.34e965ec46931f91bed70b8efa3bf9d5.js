ab22b5694155edf67fcf77f83793e454980ba60f
DaniilGorokhov <kripa0cool@mail.ru>
2020-09-10 08:48:12 +0300

    Updates

---
+++ precache-manifest.34e965ec46931f91bed70b8efa3bf9d5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9275fc7fd887fc6232f53069af9029ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "506a54ecbec736a2fbe1",
+    "url": "/static/css/main.2a8fade7.chunk.css"
+  },
+  {
+    "revision": "2acb1253f70f1995a8b8",
+    "url": "/static/js/2.95af18a6.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/static/js/2.95af18a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506a54ecbec736a2fbe1",
+    "url": "/static/js/main.b537c266.chunk.js"
+  },
+  {
+    "revision": "df11726cbf66339790ea",
+    "url": "/static/js/runtime-main.970cab50.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "dd480b0d953c2e73dd0c54d7617d391a",
+    "url": "/static/media/check-symbol.dd480b0d.svg"
+  },
+  {
+    "revision": "3e408ef227a082feb9d3527437ef78c2",
+    "url": "/static/media/edit.3e408ef2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "36053a5b28bf11d11c66adc05394d4d3",
+    "url": "/static/media/refresh-button.36053a5b.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/static/media/sub.4eb05fdd.png"
+  }
+]);
